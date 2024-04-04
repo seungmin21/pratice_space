e4f4ede85b38591c693b173fb96cfe8195c96681
Oh seung min <ohmin@gmail.com>
2024-04-04 20:43:10 +0900

    객체 지향 프로그래밍 코드 예시 추가

--- add.js
+++ add.js
@@ -9,3 +9,15 @@ console.log(b);
 // 마찬가지로 객체를 반환
 // 이유는 return에 있는 this 때문
 console.log(b.func())
+
+// 동적 객체 생성
+const obj = new Object();
+console.log(obj);
+
+function add(x, y) {
+  this.x = x;
+  this.y = y;
+}
+
+const addOne = new add(1, 2);
+console.log(addOne);
